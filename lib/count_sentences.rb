53d172df6693a64530e4c8ab3cc827e996f0c40b
Trisha Garrett <trisha.garrett@gmail.com>
2017-12-04 20:05:49 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,10 @@ class String
   end
 
   def count_sentences
-    sentence_count = self.split([.!? ])
-      sentence_count.count_sentences
+    # sentence_count = self.split()
+    #   sentence_count.count_sentences
+
+    sentence_array = self.split(/[.?!]/)
+      return sentence_array.count
   end
 end
