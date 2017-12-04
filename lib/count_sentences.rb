35390247bf6a8ab161e742be51de5b92b03de791
Trisha Garrett <trisha.garrett@gmail.com>
2017-12-04 20:08:17 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    # sentence_count = self.split()
+    #   sentence_count.count
 
+    sentence_array = self.split(/[.?!]+/)
+      return sentence_array.count
   end
 end
